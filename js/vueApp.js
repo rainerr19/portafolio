b4cd2004b0fc69c8bdcbc86fb665503a85ad5c8d
rainerr19 <rainerr19@gmail.com>
2019-06-13 21:24:59 -0500

    version 1.0

---
+++ js/vueApp.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el:'#body',
+    data:{
+        titulo:'Portafolio  en contrución ;).',
+        li:['informacion personal y redes sociales','descripción','skills','titulos',
+         'enviar propuestas' ],
+         ObjectLi:[{tex:'informacion personal y redes sociales',val:1},
+                {tex:'descripción',val:2},{tex:'skills',val:3},{tex:'titulos',val:4},
+                {tex:'enviar propuestas',val:5 }]
+    }
+})
